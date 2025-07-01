69c60bf52792d310a6d06241402ac2cf81d97581
kananpandit02 <kananpandit02@gmail.com>
2025-07-01 11:05:27 +0530

    Update firebase_config.js

--- public/firebase_config.js
+++ public/firebase_config.js
@@ -1,10 +1,11 @@
-{
-  "hosting": {
-    "public": "public",
-    "ignore": [
-      "firebase.json",
-      "**/.*",
-      "**/node_modules/**"
-    ]
-  }
-}
+// Firebase Web App Config (from your Firebase Console)
+const firebaseConfig = {
+  apiKey: "AIzaSyAwaM9fk8464I39wCGcLDGANvKyhgB__so",
+  authDomain: "student-teaching-c91d5.firebaseapp.com",
+  projectId: "student-teaching-c91d5",
+  storageBucket: "student-teaching-c91d5.appspot.com",
+  messagingSenderId: "265212909874",
+  appId: "1:265212909874:web:2cb7fdc98858d2e6aadf2b"
+};
+
+firebase.initializeApp(firebaseConfig);
