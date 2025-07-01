78a4a57c694f4be8917fca6f99e7f6c0259b9a67
kananpandit02 <kananpandit02@gmail.com>
2025-07-01 12:17:01 +0530

    Update firebase_config.js

--- public/firebase_config.js
+++ public/firebase_config.js
@@ -1,4 +1,4 @@
-// Firebase SDK v9 compat version (used with -compat libraries)
+// firebase_config.js
 const firebaseConfig = {
   apiKey: "AIzaSyAwaM9fk8464I39wCGcLDGANvKyhgB__so",
   authDomain: "student-teaching-c91d5.firebaseapp.com",
@@ -8,11 +8,4 @@ const firebaseConfig = {
   appId: "1:265212909874:web:2cb7fdc98858d2e6aadf2b"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
-// Optionally enable services globally (used in script.js)
-const auth = firebase.auth();
-const db = firebase.firestore();
-const storage = firebase.storage();
-
